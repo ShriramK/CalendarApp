3552fe38a37c462a6d6d8a24e80c5cf7801d3724
Shriram Kunchanapalli <kshriram18@gmail.com>
2013-07-28 17:32:28 +0530

    Working Code for Part II of Calendar App
    As per http://lightbird.net/dbe/cal2.html
    Pending css changes for month and day view
    Also includes Alerts portion from Part III

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('calendarapp.views',
+	(r"^day/(\d{4})/(\d+)/(\d+)/$", "day"),
 	(r"^month/(\d{4})/(\d+)/$", "month"),
+	(r"^month/(\d{4})/(\d+)/(prev|next)/$", "month"),
+	(r"^month$", "month"),
 	(r"^settings/$", "settings"),
 	(r"^(\d+)/$", "main"),
 	(r"", "main"),
--- views.py
+++ views.py
@@ -1,5 +1,10 @@
+import calendar
 import time
+from datetime import date, datetime, timedelta
 from django.contrib.auth.decorators import login_required
+from django.core.context_processors import csrf
+from django.core.urlresolvers import reverse
+from django.forms.models import modelformset_factory
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 
@@ -35,11 +40,85 @@ def main(request, year=None):
 			mlst.append(dict(n=n+1, name=month, entry=entry, current=current))
 		lst.append((y, mlst))
 
-	return render_to_response("calendarapp/main.html", dict(years=lst, user=request.user, year=year))
-													#reminders=reminders(request)))
+	return render_to_response("calendarapp/main.html", dict(years=lst, user=request.user, year=year,
+													reminders=reminders(request)))
 
 def settings(request):
 	pass
 
-def month(request, year=None, monthnum=None):
-	pass
+@login_required
+def month(request, year, month, change=None):
+	"""Listing of days in a 'month'."""
+	year, month = int(year), int(month)
+
+	# apply next / previous change
+	if change in ("next", "prev"):
+		now, mdelta = date(year, month, 15), timedelta(days=31)
+		if change == "next":
+			mod = mdelta
+		elif change == "prev":
+			mod = -mdelta
+
+		year, month = (now+mod).timetuple()[:2]
+
+	# init variables
+	cal = calendar.Calendar()
+	month_days = cal.itermonthdays(year, month)
+	nyear, nmonth, nday = time.localtime()[:3]
+	lst = [[]]
+	week = 0
+
+	# make month lists containing list of days for each week
+	# each day tuple will contain list of entries and 'current' indicator
+	for day in month_days:
+		entries = current = False # are there entries for this day; current day?
+		if day:
+			entries = Entry.objects.filter(date__year=year, date__month=month, date__day=day)
+			if day == nday and year == nyear and month == nmonth:
+				current = True
+
+		lst[week].append((day, entries, current))
+		if len(lst[week]) == 7:
+			lst.append([])
+			week += 1 
+
+	return render_to_response("calendarapp/month.html", dict(year=year, month=month, user=request.user,
+						month_days=lst, mname=mnames[month-1], reminders=reminders(request)))
+
+@login_required
+def day(request, year, month, day):
+	"""Entries for the day."""
+	EntriesFormset = modelformset_factory(Entry, extra=1, exclude=("creator", "date"), 
+										  can_delete=True)
+	if request.method == 'POST':
+		formset = EntriesFormset(request.POST)
+		if formset.is_valid():
+			# add current user and date to each entry & save
+			entries = formset.save(commit=False)
+			for entry in entries:
+				entry.creator = request.user
+				entry.date = date(int(year), int(month), int(day))
+				entry.save()
+			return HttpResponseRedirect(reverse("calendarapp.views.month", args=(year, month)))
+	else:
+		# display formset for existing entries and one extra form
+		formset = EntriesFormset(queryset=Entry.objects.filter(date__year=year, 
+			date__month=month, date__day=day, creator=request.user))
+	return render_to_response("calendarapp/day.html", add_csrf(request, entries=formset, year=year,
+				month=month, day=day))
+
+def add_csrf(request, ** kwargs):
+	"""Add CSRF and user to dictionary."""
+	d = dict(user=request.user, ** kwargs)
+	d.update(csrf(request))
+	return d
+
+def reminders(request):
+	"""Return the list of reminders for today and tomorrow."""
+	year, month, day = time.localtime()[:3]
+	reminders = Entry.objects.filter(date__year=year, date__month=month,
+									date__day=day, creator=request.user, remind=True)
+	tomorrow = datetime.now() + timedelta(days=1)
+	year, month, day = tomorrow.timetuple()[:3]
+	return list(reminders) + list(Entry.objects.filter(date__year=year, date__month=month,
+									date__day=day, creator=request.user, remind=True))
